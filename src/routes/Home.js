9eadccb66ffab34e349670306d905e405daf6e10
Eleke Michael <103263178+Micheal-18@users.noreply.github.com>
2025-04-28 21:31:12 +0100

    Add files via upload

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../components/Navbar"
+import HeroImg from "../components/Heroimg"
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroImg />
+    </div>
+  );
+};
+
+export default Home;
